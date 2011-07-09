26e61102f19c2a255cff4158d15394023e1f81ee
agressog@gmail.com <agressog@gmail.com@248aaad9-1297-9a1b-d59b-12ff808e70ab>
2011-07-09 20:34:04 +0000

    Added filling of new chunks. To-file function now in file tofile.cpp.

--- trunk/png_fuzzer/Chunks.cpp
+++ trunk/png_fuzzer/Chunks.cpp
@@ -1,8 +1,5 @@
 #include "stdafx.h"
 #include "Chunks.h"
-#include <string>
-#include <fstream>
-#include <iostream>
 
 using namespace std;
 
@@ -13,10 +10,13 @@ chunk::chunk()
 	char sRGBfield1 [8]={0,0,0,1,115,82,71,66};
 	char gAMAfield1 [8]={0,0,0,4,103,65,77,65};
 	char pHYSfield1 [8]={0,0,0,9,112,72,89,115};
+	char cHRMfield1 [8]={0,0,0,32,99,72,82,77};//new
 	char PLTEfield1 [8]={0,0,0,3,80,76,84,69};
+	char hISTfield1 [8]={0,0,0,2,104,73,83,84};
 	char IDATfield1 [8]={0,0,0,12,73,68,65,84};
 	char IDATdata1 [12]={24,87,99,248,255,255,63,0,5,254,2,254};
 	char IENDfield1 [8]={0,0,0,0,73,69,78,68};
+//All fields (names) of chunks
 	for (int i=0;i<8;i++)
 	{
 	sign[i]=sign1[i];
@@ -24,30 +24,46 @@ chunk::chunk()
 	sRGBfield[i]=sRGBfield1 [i];
 	gAMAfield[i]=gAMAfield1 [i];
 	pHYSfield[i]=pHYSfield1 [i];
+	cHRMfield[i]=cHRMfield1[i];
 	PLTEfield[i]=PLTEfield1 [i];
+	hISTfield[i]=hISTfield1 [i];
 	IDATfield[i]=IDATfield1 [i];
 	IENDfield[i]=IENDfield1 [i];
-
 	}
+//Filling data
 	for (int i=0;i<12;i++)
 	{
 	IDATdata [i]=IDATdata1 [i];
 	}
 
-	char IENDCRC1[4]={174,66,96,130};
-	IHDRCompression=0;
-	IHDRFilter=0;
+	char IENDCRC1[4]={174,66,96,130};//CRC of IEND is constant
+
 	char IHDRWidth1[4]={0,0,0,1};
 	char IHDRHeight1[4]={0,0,0,1};
+	//
 	char gAMAgamma1[4]={0,0,0,1};
+	//
 	char pHYSX1[4]={0,0,14,195};
 	char pHYSY1[4]={0,0,14,195};
-
+	//
+	char cHRMWhiteX1 [4]={0,0,0,1};
+	char cHRMWhiteY1 [4]={0,0,0,1};
+	char cHRMRedX1 [4]={0,0,0,1};
+	char cHRMRedY1 [4]={0,0,0,1};
+	char cHRMGreenX1 [4]={0,0,0,1};
+	char cHRMGreenY1 [4]={0,0,0,1};
+	char cHRMBlueX1 [4]={0,0,0,1};
+	char cHRMBlueY1 [4]={0,0,0,1};
+//Filling CRC's
 	char IHDRCRC1[4]={0,0,0,1};
 	char sRGBCRC1[4]={0,0,0,1};
 	char gAMACRC1[4]={0,0,0,1};
+	char cHRMCRC1[4]={0,0,0,1};
+	char PLTECRC1[4]={0,0,0,1};
+	char hISTCRC1[4]={0,0,0,1};
 	char pHYSCRC1[4]={0,0,0,1};
 	char IDATCRC1[4]={0,0,0,1};
+
 
 
 	for (int i=0;i<4;i++)
@@ -61,10 +77,25 @@ chunk::chunk()
 		sRGBCRC[i]=sRGBCRC1[i];
 		gAMACRC[i]=gAMACRC1[i];
 		pHYSCRC[i]=pHYSCRC1[i];
+		cHRMWhiteX[i]=cHRMWhiteX1[i];
+		cHRMWhiteY[i]=cHRMWhiteY1[i];
+		cHRMRedX[i]=cHRMRedX1[i];
+		cHRMRedY[i]=cHRMRedY1[i];
+		cHRMGreenX[i]=cHRMGreenX1[i];
+		cHRMGreenY[i]=cHRMGreenY1[i];
+		cHRMBlueX[i]=cHRMBlueX1[i];
+		cHRMBlueY[i]=cHRMBlueY1[i];
+		cHRMCRC[i]=cHRMCRC1[i];
+		PLTECRC[i]=PLTECRC1[i];
+		hISTCRC[i]=hISTCRC1 [i];
 		IDATCRC[i]=IDATCRC1[i];
 		IENDCRC[i]=IENDCRC1[i];
 	}
-
+//Filling one-byte fields
+	hISTFreq[0]=0;
+	hISTFreq[1]=1;
+	IHDRCompression=0;
+	IHDRFilter=0;
 	IHDRColour=0;
 	IHDRDepth=1;
 	IHDInterlace=0;
@@ -77,83 +108,4 @@ chunk::chunk()
 		PLTERedGreenBBlue[i]=PLTERedGreenBBlue1[i];
 	}
 	
-}
-
-
-void chunk::tofile()
-{
-	FILE *f;
-	char b [256];
-	int i;
-	int temp;
-
-	for (i=0;i<8;i++)
-		b[i]=sign[i];
-
-	for (i=8;i<16;i++)
-		b[i]=IHDRfield[i-8];
-	for (i=16;i<20;i++)
-		b[i]=IHDRWidth[i-16];
-	for (i=20;i<24;i++)
-		b[i]=IHDRHeight[i-20];
-		b[i++]=IHDRDepth;
-		b[i++]=IHDRColour;
-		b[i++]=IHDRCompression;
-		b[i++]=IHDRFilter;
-		b[i++]=IHDInterlace;
-		for (temp=i;i<temp+4;i++)
-			b[i]=IHDRCRC[i-temp];
-
-		for(temp=i;i<temp+8;i++)
-			b[i]=sRGBfield [i-temp];
-		b[i++]=sRGBRendering;
-		for (temp=i;i<temp+4;i++)
-			b[i]=sRGBCRC[i-temp];
-
-		for(temp=i;i<temp+8;i++)
-			b[i]=gAMAfield [i-temp];
-		for (temp=i;i<temp+4;i++)
-			b[i]=gAMAgamma[i-temp];
-		for (temp=i;i<temp+4;i++)
-			b[i]=gAMACRC[i-temp];
-
-		for(temp=i;i<temp+8;i++)
-			b[i]=pHYSfield [i-temp];
-		for (temp=i;i<temp+4;i++)
-			b[i]=pHYSX[i-temp];
-		for (temp=i;i<temp+4;i++)
-			b[i]=pHYSY[i-temp];
-		b[i++]=pHYSspecifier;
-		for (temp=i;i<temp+4;i++)
-			b[i]=pHYSCRC[i-temp];
-
-		for(temp=i;i<temp+8;i++)
-			b[i]=PLTEfield [i-temp];
-		for (temp=i;i<temp+3;i++)
-			b[i]=PLTERedGreenBBlue[i-temp];
-		for (temp=i;i<temp+4;i++)
-			b[i]=PLTECRC[i-temp];
-
-		for(temp=i;i<temp+8;i++)
-			b[i]=IDATfield [i-temp];
-		for (temp=i;i<temp+12;i++)
-			b[i]=IDATdata[i-temp];
-		for (temp=i;i<temp+4;i++)
-			b[i]=IDATCRC[i-temp];
-		
-		for(temp=i;i<temp+8;i++)
-			b[i]=IENDfield [i-temp];
-		for (temp=i;i<temp+4;i++)
-			b[i]=IENDCRC[i-temp];
-	
-	//a=a+IHDRfield;
-	//a=sign+(string)IHDRfield;
-	//cout<<sign;
-	cin.get();
-    ofstream file("test1.png");
-	f=fopen("d://2.png","rb+");
-	fwrite(b,i*sizeof(char),1,f);
-	//for(i=0;i<8;i++)
-	//char c=a[5];
-	//file<<c;
 }
