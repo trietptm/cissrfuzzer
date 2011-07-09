31b793fd7bbf70a1cf690bdfc4a741056c627f32
nazarenkoff <nazarenkoff@gmail.com>
2011-07-09 19:37:19 +0000

    increased version of os\2 table, fixed offsets for table directory nods in fuction generate_table() in mane.cpp

--- font_fuzzer/os_2_table.cpp
+++ font_fuzzer/os_2_table.cpp
@@ -2,7 +2,7 @@
 void os_2_table::printTable(char* path){
       ofstream file;
       file.open(path,ios::binary|ios::app);
-      file<<(char)0<<(char)0;//version
+      file<<(char)0<<(char)4;//version
       file<<(char)(xAvgCharWidth>>8)<<(char)(xAvgCharWidth%256);
       file<<(char)(usWeightClass>>8)<<(char)(usWeightClass%256);
       file<<(char)(usWidthClass>>8)<<(char)(usWidthClass%256);
@@ -14,7 +14,6 @@ void os_2_table::printTable(char* path){
       file<<(char)(ySuperscriptXSize>>8)<<(char)(ySuperscriptXSize%256);
       file<<(char)(ySuperscriptYSize>>8)<<(char)(ySuperscriptYSize%256);
       file<<(char)(ySuperscriptXOffset>>8)<<(char)(ySuperscriptXOffset%256);
-      file<<(char)(ySuperscriptYOffset>>8)<<(char)(ySuperscriptYOffset%256);
       file<<(char)(ySuperscriptYOffset>>8)<<(char)(ySuperscriptYOffset%256);
       file<<(char)(yStrikeoutSize>>8)<<(char)(yStrikeoutSize%256);
       file<<(char)(yStrikeoutPosition>>8)<<(char)(yStrikeoutPosition%256);
@@ -28,10 +27,25 @@ void os_2_table::printTable(char* path){
       file<<(char)(fsSelection>>8)<<(char)(fsSelection%256);
       file<<(char)(fsFirstCharIndex>>8)<<(char)(fsFirstCharIndex%256);
       file<<(char)(fsLastCharIndex>>8)<<(char)(fsLastCharIndex%256);
+      //for version 4
+      file<<(char)(sTypoAscender>>8)<<(char)(sTypoAscender%256);
+      file<<(char)(sTypoDescender>>8)<<(char)(sTypoDescender%256);
+      file<<(char)(sTypoLineGap>>8)<<(char)(sTypoLineGap%256);
+      file<<(char)(usWinAscent>>8)<<(char)(usWinAscent%256);
+      file<<(char)(usWinDescent>>8)<<(char)(usWinDescent%256);
+      file<<(char)(ulCodePageRange1>>24)<<(char)((ulCodePageRange1>>16)&255);
+      file<<(char)((ulCodePageRange1>>8)&255)<<(char)(ulCodePageRange1%256); 
+      file<<(char)(ulCodePageRange2>>24)<<(char)((ulCodePageRange2>>16)&255);
+      file<<(char)((ulCodePageRange2>>8)&255)<<(char)(ulCodePageRange2%256); 
+      file<<(char)(sxHeight>>8)<<(char)(sxHeight%256);
+      file<<(char)(sCapHeight>>8)<<(char)(sCapHeight%256);
+      file<<(char)(usDefaultChar>>8)<<(char)(usDefaultChar%256);
+      file<<(char)(usBreakChar>>8)<<(char)(usBreakChar%256);
+      file<<(char)(usMaxContext>>8)<<(char)(usMaxContext%256);
       file.close();
 }
 uint32 os_2_table::getSize(){
-       return 78;
+       return 96;
 }
 void gener_os_2_table(os_2_table &ost){
      ost.version=0;
@@ -66,4 +80,17 @@ void gener_os_2_table(os_2_table &ost){
      ost.fsSelection=rand()%6;
      ost.fsFirstCharIndex=rand()%64;
      ost.fsLastCharIndex=ost.fsFirstCharIndex+rand()%256;
+     //for ver. 4
+     ost.sTypoAscender=rand()%64;
+     ost.sTypoDescender=rand()%64;
+     ost.sTypoLineGap=rand()%64;
+     ost.usWinAscent=rand()%64;
+     ost.usWinDescent=rand()%64;
+     ost.ulCodePageRange1=((rand()%64)<<16)+(rand()%256); 
+     ost.ulCodePageRange2=(rand()%65535)<<16;
+     ost.sxHeight=rand()%64;	 
+     ost.sCapHeight=rand()%64;	
+     ost.usDefaultChar=rand()%0xffff; //<0xffff
+     ost.usBreakChar=rand()%0xffff;	 //<0xffff
+     ost.usMaxContext=rand()%10; 
 }
