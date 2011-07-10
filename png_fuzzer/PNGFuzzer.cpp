26429ebd62eb1cc9845e71572e0b6774824f2ba9
agressog <agressog@gmail.com>
2011-07-10 19:35:44 +0000

    Main function for programm.

---
+++ png_fuzzer/PNGFuzzer.cpp
@@ -0,0 +1,15 @@
+#include "stdafx.h"
+#include "Chunks.h"
+#include <iostream>
+
+using namespace std;
+
+
+int _tmain(int argc, _TCHAR* argv[])
+{
+	chunk b;
+	b.tofile();
+	cin.get();
+	return 0;
+}
+
