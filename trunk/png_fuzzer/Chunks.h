cf4c3516237318d0dccf44335d02485d462e09c2
agressog@gmail.com <agressog@gmail.com@248aaad9-1297-9a1b-d59b-12ff808e70ab>
2011-07-10 19:33:20 +0000

    Added new chunks, size of "data" is not constant.

--- trunk/png_fuzzer/Chunks.h
+++ trunk/png_fuzzer/Chunks.h
@@ -6,6 +6,7 @@ class chunk
 {
 public:
 	chunk();
+	~chunk(){delete [] IDATdata;}
 	void tofile();
 private:
 	char sign[8];
@@ -43,28 +44,43 @@ private:
 	char cHRMBlueX [4];
 	char cHRMBlueY [4];
 	char cHRMCRC [4];
-	/*unsigned int cHRMWhiteX;
-	unsigned intcHRMWhiteY;
-	unsigned int cHRMRedX;
-	unsigned int cHRMRedY;
-	unsigned int cHRMGreenX;
-	unsigned int cHRMGreenY;
-	unsigned int cHRMBlueX;
-	unsigned int cHRMBlueY;
-	char cHRMCRC [4];*/
 
+	char sBITfield [8];//Significant bits
+	char sBITGrey; 
+	char sBITRed; 
+	char sBITGreen; 
+	char sBITBlue; 
+	char sBITAlpha; 
+	char sBITCRC [4];
 
+	char bKGDfield [8];//Background colour
+	char bKGDgreyscale [2];
+	char bKGDred [2];
+	char bKGDgreen [2];
+	char bKGDblue [2];
+	char bKGDpalette;
+	char bKGDCRC [4];
 
 	char PLTEfield [8];//Palette
 	char PLTERedGreenBBlue[3];//from 1 to 256
 	char PLTECRC [4];
+
+	char tRNSfield [8];
+	char tRNSgrey [2];
+	char tRNSred [2];
+	char tRNSgreen [2];
+	char tRNSblue [2];
+	char tRNSone;
+	char tRNStwo;
+	char tRNSCRC [4];
 
 	char hISTfield [8];//Image histogram
 	char hISTFreq[2];
 	char hISTCRC [4];
 
 	char IDATfield [8];//Image data
-	char IDATdata[12];//data from example file
+	char * IDATdata;//data from example file
+	int IDATsize;
 	char IDATCRC [4];
 
 	char IENDfield [8];
